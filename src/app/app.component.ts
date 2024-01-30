05832a6009c994df3159ea9a210c228c8ee3742f
MichelHdez <michel01406@gmail.com>
2024-01-29 18:14:44 -0600

    Proyecto Piano Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Piano';
+  aplicarSonido(numero:number): void{
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/note' + numero + '.wav';
+    audio.load();
+    audio.play();
+  }
 }
